dcb137edcb005dc0157193d968e1f863d6c73d52
Balı <75204782+abdbali@users.noreply.github.com>
2025-11-18 15:26:21 +0300

    Create page.jsx

---
+++ app/page.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Stepper from '../components/Stepper'
+import './globals.css'
+
+export default function Page() {
+  return (
+    <main className="root">
+      <div className="center">
+        <Stepper />
+      </div>
+    </main>
+  )
+}
